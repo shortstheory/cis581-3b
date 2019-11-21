d481bb6677466af6f51c95134e1de8c6dea88814
Saumya-Shah <you@example.com>
2019-11-21 03:36:25 -0500

    edited interp2

--- estimateFeatureTranslation.py
+++ estimateFeatureTranslation.py
@@ -1,6 +1,55 @@
 import numpy as np
 import cv2
 
+def interp2(v, xq, yq):
+    if len(xq.shape) == 2 or len(yq.shape) == 2:
+        dim_input = 2
+        q_h = xq.shape[0]
+        q_w = xq.shape[1]
+        xq = xq.flatten()
+        yq = yq.flatten()
+
+    h = v.shape[0]
+    w = v.shape[1]
+    if xq.shape != yq.shape:
+        raise 'query coordinates Xq Yq should have same shape'
+
+    x_floor = np.floor(xq).astype(np.int32)
+    y_floor = np.floor(yq).astype(np.int32)
+    x_ceil = np.ceil(xq).astype(np.int32)
+    y_ceil = np.ceil(yq).astype(np.int32)
+
+    x_floor[x_floor < 0] = 0
+    y_floor[y_floor < 0] = 0
+    x_ceil[x_ceil < 0] = 0
+    y_ceil[y_ceil < 0] = 0
+
+    x_floor[x_floor >= w-1] = w-1
+    y_floor[y_floor >= h-1] = h-1
+    x_ceil[x_ceil >= w-1] = w-1
+    y_ceil[y_ceil >= h-1] = h-1
+
+    v1 = v[y_floor, x_floor]
+    v2 = v[y_floor, x_ceil]
+    v3 = v[y_ceil, x_floor]
+    v4 = v[y_ceil, x_ceil]
+
+    lh = yq - y_floor
+    lw = xq - x_floor
+    hh = 1 - lh
+    hw = 1 - lw
+
+    w1 = hh * hw
+    w2 = hh * lw
+    w3 = lh * hw
+    w4 = lh * lw
+
+    interp_val = v1 * w1 + w2 * v2 + w3 * v3 + w4 * v4
+
+    if dim_input == 2:
+        return interp_val.reshape(q_h, q_w)
+    return interp_val
+
 def estimateFeatureTranslation(startX, startY, Ix, Iy, img1, img2):
     i=0
     img1G = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
@@ -22,7 +71,7 @@ def estimateFeatureTranslation(startX, startY, Ix, Iy, img1, img2):
     Iyp = Iy[nC1[0],nC1[1]].reshape(-1,1)
     A = np.hstack([Ixp,Iyp])
     while ((u+v)>1 and i<15):
-        It = -img2G[nC1[0],nC1[1]]+img1G[nC2[0],nC2[1]]
+        It = -interp2(img2G,nC2[0],nC2[1])+img1G[nC1[0],nC1[1]]
         u,v = np.linalg.inv(A.T.dot(A)).dot(A.T).dot(It)
         nC2[0] = nC2[0]+v
         nC2[1] = nC2[1]+u
