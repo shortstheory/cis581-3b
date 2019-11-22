05eae97be25413e310233390502a5106487b0a35
arnav dhamija <arnav.dhamija@gmail.com>
2019-11-22 00:15:06 -0500

    added bbox+tracking

--- applyBoxTransform.py
+++ applyBoxTransform.py
@@ -1,9 +1,16 @@
 import numpy as np
 import cv2
 from utils import *
+from ransac_est_homography import ransac_est_homography
 
 def applyBoxTransform(startX, startY, newX, newY, box):
-    H = est_homography(startX,startY,newX,newY)
+    # u = np.mean(newX-startX)
+    # v = np.mean(newY-startY)
+    # box[0] += u
+    # box[1] += v
+    # box[2] += u
+    # box[3] += v
+    H = ransac_est_homography(startX,startY,newX,newY,0.001)
     n = startX.shape[0]
     stk = np.vstack((startX,startY,np.ones(n)))
     res = H@stk
@@ -16,8 +23,11 @@ def applyBoxTransform(startX, startY, newX, newY, box):
     xmax = box[2]
     ymin = box[1]
     ymax = box[3]
-    boxStk = [[xmin,xmax],[ymin,ymax],[1,1]]
-    newBox = H@boxStk
-    newBox = newBox/newBox[2:]
-    _box = np.array([newBox[0,0],newBox[1,0],newBox[0,1],newBox[1,1]])
-    return X,Y,_box
+    boxStk = [[xmin,xmax,xmin,xmax],
+              [ymin,ymin,ymax,ymax],
+              [1,1,1,1]]
+    boxPts = H@boxStk
+    boxPts = boxPts/boxPts[2:]
+    # print(boxPts)
+    box = np.array([np.min(boxPts[0,:]),np.min(boxPts[1,:]),np.max(boxPts[0,:]),np.max(boxPts[1,:])])
+    return newX,newY,box
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from matplotlib import pyplot as plt
+from matplotlib import patches, pyplot, pyplot as plt
 import cv2
 import xml.etree.ElementTree as ET 
 import numpy as np
@@ -8,6 +8,7 @@ from getBoxFeatures import *
 from corner_detector import *
 from anms import *
 from estimateAllTranslation import *
+from applyBoxTransform import applyBoxTransform
 
 cap = cv2.VideoCapture('vids/Easy.mp4')
 ret,firstFrame = cap.read()
@@ -22,26 +23,39 @@ for box in boxes:
     boxData = {
         'coords': box,
         'x': x,
-        'y': y
+        'y': y,
+        'startX': x.copy(),
+        'startY': y.copy(),
+        'startCoords': box
     }
     boxesData.append(boxData)
-
+print(boxesData)
 currentFrame = firstFrame
 idx = 0
+
 while(cap.isOpened()):
     ret,nextFrame = cap.read()
     if ret == False:
         break
     # do work
     gray = cv2.cvtColor(currentFrame,cv2.COLOR_BGR2GRAY)
-
-    plt.imshow(cv2.cvtColor(nextFrame,cv2.COLOR_BGR2GRAY))
+    plt.imshow(cv2.cvtColor(nextFrame,cv2.COLOR_BGR2RGB))
     fig = plt.gcf()
 
     for boxData in boxesData:
-        boxData['x'],boxData['y'] = estimateAllTranslation(boxData['x'],boxData['y'],currentFrame,nextFrame)
+        X,Y = estimateAllTranslation(boxData['x'],boxData['y'],currentFrame,nextFrame)
+        startX = boxData['startX']
+        startY = boxData['startY']
+        # boxData['x'],boxData['y'],boxData['coords'] = applyBoxTransform(boxData['x'],boxData['y'],X,Y,boxData['coords'])
+        boxData['x'],boxData['y'],boxData['coords'] = applyBoxTransform(startX,startY,X,Y,boxData['startCoords'])
         ax1 = fig.add_subplot(111)
         ax1.scatter(boxData['x'],boxData['y'],c='r',s=1)
+        coords = boxData['coords']
+        # xmin,ymin,xmax,ymax
+        rect = patches.Rectangle((coords[0],coords[1]),coords[2]-coords[0],coords[3]-coords[1],linewidth=1,edgecolor='b',facecolor='none')
+        ax1.add_patch(rect)
     plt.savefig("outputs/"+str(idx).zfill(4)+".png")
+    fig.clf()
     idx = idx+1
+    print(idx)
     currentFrame = nextFrame
