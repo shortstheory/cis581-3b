5d7b6975de3427b00ab68976fa60b1e46e160b25
Saumya-Shah <you@example.com>
2019-11-23 21:03:11 -0500

    see the Using Main notebook

--- estimateAllTranslation.py
+++ estimateAllTranslation.py
@@ -21,5 +21,5 @@ def estimateAllTranslation(startXs,startYs,valid,img1,img2):
                 xdelta = newXs[i]-startXs[i]
                 ydelta = newYs[i]-startYs[i]
                 res = np.sqrt(np.square(ydelta)+np.square(xdelta))
-                valid[i] = res < 100
+                valid[i] = res < 10
     return newXs,newYs,valid
