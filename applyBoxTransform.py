155109338cb716ff98c51d8b48aeff93ca9d93eb
arnav dhamija <arnav.dhamija@gmail.com>
2019-11-23 20:16:01 -0500

    Found better defaults

--- applyBoxTransform.py
+++ applyBoxTransform.py
@@ -3,24 +3,15 @@ import cv2
 from utils import *
 from ransac_est_homography import ransac_est_homography
 
-def applyBoxTransform(startX, startY, newX, newY, box, valid):
-    shiftedBox = np.array([0,0,0,0])
+def applyBoxTransform(prevX, prevY, newX, newY, coords, valid):
+    prevX = prevX[valid]
+    prevY = prevY[valid]
     newX = newX[valid]
     newY = newY[valid]
-    startX = startX[valid]
-    startY = startY[valid]
-    H = ransac_est_homography(startX,startY,newX,newY,0.001)
-    xmin = box[0]
-    xmax = box[2]
-    ymin = box[1]
-    ymax = box[3]
-    boxStk = [[xmin,xmax,xmin,xmax],
-            [ymin,ymin,ymax,ymax],
-            [1,1,1,1]]
+    H = ransac_est_homography(prevX,prevY,newX,newY,0.001)
+    coordsT = coords.T
+    boxStk = np.vstack((coordsT,np.ones(4)))
     boxPts = H@boxStk
     boxPts = boxPts/boxPts[2:]
-    # print(boxPts)
-    shiftedBox = np.array([np.min(boxPts[0,:]),np.min(boxPts[1,:]),np.max(boxPts[0,:]),np.max(boxPts[1,:])])
-    # xmin,ymin,xmax,ymax
-
+    shiftedBox = boxPts[0:2,:].T
     return shiftedBox
--- main.py
+++ main.py
@@ -9,7 +9,10 @@ from corner_detector import *
 from anms import *
 from estimateAllTranslation import *
 from applyBoxTransform import applyBoxTransform
-
+from shapely.geometry.polygon import LinearRing, Polygon
+from numpy import linalg
+# white car - corner 3
+# black car - corner 3
 cap = cv2.VideoCapture('vids/Easy.mp4')
 ret,firstFrame = cap.read()
 boxes = getBoundingBoxes('first.xml')
@@ -23,20 +26,22 @@ for box in boxes:
     ymax = box[3]
     boximg = gray[ymin:ymax,xmin:xmax]    
     x,y = getBoxFeatures(boximg,box,10)
-    coords = np.array([[xmin,ymin],[xmax,ymin],[xmax,ymax],[xmin,ymax]])
+    boxCoords = np.array([[xmin,ymin],[xmax,ymin],[xmax,ymax],[xmin,ymax]])
     boxData = {
-        'coords': coords,
+        'coords': boxCoords,
         'x': x,
         'y': y,
-        'startX': x.copy(),
-        'startY': y.copy(),
-        'startCoords': box,
-        'valid': np.ones(x.shape[0],dtype='bool')
+        'valid': np.ones(x.shape[0],dtype='bool'),
+        'prevBoxCoords': np.array([[0,0],[0,0],[0,0],[0,0]]),
+        'displayHeight': 0,
+        'displayWidth': 0,
+        'displayCorner': np.array([0,0])
     }
     boxesData.append(boxData)
 currentFrame = firstFrame
 idx = 0
-boxUpdateRate = 30
+boxUpdateRate = 10
+
 while(cap.isOpened()):
     ret,nextFrame = cap.read()
     if ret == False:
@@ -45,45 +50,44 @@ while(cap.isOpened()):
     gray = cv2.cvtColor(currentFrame,cv2.COLOR_BGR2GRAY)
     plt.imshow(cv2.cvtColor(nextFrame,cv2.COLOR_BGR2RGB))
     fig = plt.gcf()
-
     for boxData in boxesData:
-        # boxData['startX'] = boxData['x']
-        # boxData['startY'] = boxData['y']
+        prevX = boxData['x']
+        prevY = boxData['y']
         boxData['x'],boxData['y'],boxData['valid'] = estimateAllTranslation(boxData['x'],boxData['y'],boxData['valid'],currentFrame,nextFrame)
-        
-        # if np.sum(boxData['valid'])<8:
-        # x,y = getBoxFeatures(boximg,box,20)
-        # x = boxData['x']
-        # y = boxData['y']
-        # coords = boxData['coords']
-
-
-        # if idx % 150 == 0:
-        # boxData['startCoords'] = boxData['coords']
-        startX = boxData['startX']
-        startY = boxData['startY']
-            # boxData = {
-            #     'coords': coords,
-            #     'x': x,
-            #     'y': y,
-            #     'startX': x.copy(),
-            #     'startY': y.copy(),
-            #     'startCoords': coords,
-            #     'valid': np.ones(x.shape[0],dtype='bool')
-            # }
-
-
         ax1 = fig.add_subplot(111)
         ax1.scatter(boxData['x'][boxData['valid']],boxData['y'][boxData['valid']],c='r',s=1)
-        print(np.sum(boxData['valid']),end=" ")
         if np.sum(boxData['valid']) >= 4:
-            # if idx%10==0:
-            boxData['coords'] = applyBoxTransform(startX,startY,boxData['x'],boxData['y'],boxData['startCoords'],boxData['valid'])
+            boxData['coords'] = applyBoxTransform(prevX,prevY,boxData['x'],boxData['y'],boxData['coords'],boxData['valid'])
             coords = boxData['coords']
-            # xmin,ymin,xmax,ymax
-            if coords[0] > 0 and coords[1] > 0 and coords[2] < currentFrame.shape[1] and coords[3] < currentFrame.shape[0]:
-                rect = patches.Rectangle((coords[0],coords[1]),coords[2]-coords[0],coords[3]-coords[1],linewidth=1,edgecolor='b',facecolor='none')
-                ax1.add_patch(rect)
+            delta = linalg.norm(coords-boxData['prevBoxCoords'],axis=1)
+            minDeltaIdx = np.argmin(delta)
+            print(delta)
+            print(minDeltaIdx)
+            boxData['prevBoxCoords'] = coords
+            height = min(coords[3,1]-coords[0,1],coords[2,1]-coords[1,1])
+            width = min(coords[1,0]-coords[0,0],coords[2,0]-coords[3,1])
+            minDeltaIdx = 1
+            if minDeltaIdx == 0:
+                height = height
+                width = width
+            elif minDeltaIdx == 1:
+                height = height
+                width = -width
+            elif minDeltaIdx == 2:
+                height = -height
+                width = -width
+            elif minDeltaIdx == 3:
+                height = -height
+                width = width
+            if idx % 1 == 0:
+                boxData['displayCorner'] = coords[minDeltaIdx]
+                boxData['displayHeight'] = height
+                boxData['displayWidth']= width
+            rect = patches.Rectangle(boxData['displayCorner'],boxData['displayWidth'],boxData['displayHeight'],linewidth=1,edgecolor='r',facecolor='none')
+            ax1.add_patch(rect)
+            ring = LinearRing(coords)
+            # x,y = ring.xy
+            # ax1.plot(x, y)
     plt.savefig("outputs2/"+str(idx).zfill(4)+".png")
     fig.clf()
     idx = idx+1
